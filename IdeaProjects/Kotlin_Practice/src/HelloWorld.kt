f95ae7da484e8b4f0adddd3b75ac8c8ebf122329
sadikuls <sadikul.bari2209@gmail.com>
2018-04-22 11:10:40 +0600

    Create: Helloworld.kt print first messase with kotlin

---
+++ IdeaProjects/Kotlin_Practice/src/HelloWorld.kt
@@ -0,0 +1,6 @@
+
+//printing hello from kotlin without creating any class kotlin will create class inside
+fun main(args: Array<String>){
+    //print first line in kotlin
+    println("Hello from kotlin")
+}
